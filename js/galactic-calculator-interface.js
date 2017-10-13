15e52ee1b3999bbdda3bd8d5fc090bd05c3f60ce
mo <meganannetteolson@yahoo.com>
2017-10-13 15:41:09 -0700

    fix spacing on earth div

--- js/galactic-calculator-interface.js
+++ js/galactic-calculator-interface.js
@@ -17,11 +17,11 @@ $(document).ready(function(){
     }
 
     if (ageInput.toString().length <= 3) {
-       $(".earth").text("Your age on Earth is: " + ageInput));
-       $(".earthFuture").text(" Your average life expectancy on Earth is: " + calculator.earthExpectancyCalculator(ageInput, gender));
+       $(".earth").text("Your age on Earth is: " + ageInput);
+       $(".earthFuture").text("Your average life expectancy on Earth is: " + calculator.earthExpectancyCalculator(ageInput, gender));
      } else if (ageInput.toString().length === 4) {
        $(".earth").text("Your age on Earth is: " + calculator.ageCalculator(ageInput));
-       $(".earthFuture").text(" Your average life expectancy on Earth is: " + calculator.earthExpectancyCalculator(ageInput, gender));
+       $(".earthFuture").text("Your average life expectancy on Earth is: " + calculator.earthExpectancyCalculator(ageInput, gender));
      } else if (ageInput.toString().length > 4){
        $(".error").text("Please enter a valid age.");
        $(".output").addClass("hide");
